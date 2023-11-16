0d24c774ce53187820ce60c987518ad428a4636d
vishaldhakal <vishaldhakal96@gmail.com>
2023-11-16 21:32:06 +0545

    dfV

--- app/page.js
+++ app/page.js
@@ -363,6 +363,19 @@ export default async function Home() {
         </div>
         <div className="container-fluid">
           <DolphyAdvantage></DolphyAdvantage>
+          <div className="py-md-5"></div>
+          <div className="py-5">
+            <h3 className="fs-2 text-mine fw-bold text-center mb-0">
+              We bring you home from credible builders
+            </h3>
+            <div className="d-flex flex-column align-items-center justify-content-center">
+              <img
+                src="/builders.png"
+                alt="builders in dolphy"
+                className="img-fluid mt-3 w-100 w-md-75"
+              />
+            </div>
+          </div>
           <div className="py-5 my-5" id="mycontact">
             <div className="container-fluid">
               <div className="row justify-content-center">
--- components/DolphyAdvantage.js
+++ components/DolphyAdvantage.js
@@ -5,21 +5,11 @@ export default function DolphyAdvantage() {
         <h3 className="fs-2">
           <strong>The Dolphy Advantage</strong> - Always people at heart
         </h3>
-        <p>Working with leading home & condos developers & industry partners</p>
+        <p>
+          Working hand in hand with leading Pre construction Homes, Condos
+          Developers & Industry Partners
+        </p>
         <img src="/team.png" alt="dolphy team" className="img-fluid" />
-      </div>
-      <div className="py-md-5"></div>
-      <div className="py-5">
-        <h3 className="fs-2 text-mine fw-bold text-center mb-0">
-          We bring you home from credible builders
-        </h3>
-        <div className="d-flex flex-column align-items-center justify-content-center">
-          <img
-            src="/builders.png"
-            alt="builders in dolphy"
-            className="img-fluid mt-3 w-100 w-md-75"
-          />
-        </div>
       </div>
     </>
   );

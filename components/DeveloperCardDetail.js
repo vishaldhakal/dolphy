1c8ba3d87a033f5e31f379f9be8799631366a013
vishaldhakal <vishaldhakal96@gmail.com>
2023-11-26 21:01:12 +0545

    dfv

--- components/DeveloperCardDetail.js
+++ components/DeveloperCardDetail.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function DeveloperCardDetail(props) {
   return (
     <>
-      <div className="card border-0 rounded-mine my-2 my-md-0 pb-5 condocard">
-        <div className="position-relative is-loading">
+      <div className="card border-0 rounded-mine my-2 my-md-0 pb-5 condocard position-relative mt-5 max-w-mine">
+        <div className="builder-logo-detail is-loading">
           {props.image ? (
             <img
               loading="lazy"
@@ -22,9 +22,12 @@ export default function DeveloperCardDetail(props) {
             />
           )}
         </div>
-        <div className="card-content pt-2 px-4 developer-desc">
-          <h3 className="mb-1 cardd-title text-dark">{props.name}</h3>
-          <h4 className="mb-2 cardd-subtitle">
+        <div className="mt-4"></div>
+        <div className="card-content px-4 developer-desc mt-5">
+          <h3 className="mb-1 cardd-title text-dark text-center">
+            {props.name}
+          </h3>
+          <h4 className="mb-2 cardd-subtitle text-center">
             <span className="link-black">Website :</span> {props.website_link}
           </h4>
           {/* {props.phone !== "n/a" ? (
@@ -37,7 +40,7 @@ export default function DeveloperCardDetail(props) {
               <b className="ms-1">Phone number not available</b>
             </>
           )} */}
-          <p className="mb-0 pt-3">{props.details}</p>
+          <p className="mb-0 pt-3 text-center">{props.details}</p>
         </div>
       </div>
     </>
